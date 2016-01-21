bef80dd4cdff57b5ba15b9751b1f4c13c0497cac
Ahmet ATAR <ahmetatar@outlook.com>
2016-01-21 10:51:04 +0200

    First commit

---
+++ TemplateEnginesPOC/Scripts/utils.js
@@ -0,0 +1,37 @@
+﻿Stopwatch = function () {
+
+    var self = this;
+    var isRunning = false;
+    var capturedTime = null;
+
+    function getTime() {
+        var d = new Date();
+        return d.getTime();
+    }
+
+    self.ElapsedTime = null;
+
+    self.start = function () {
+        if (isRunning) {
+            return;
+        }
+
+        self.reset();
+
+        isRunning = true;
+        capturedTime = getTime();
+    }
+
+    self.stop = function () {
+        isRunning = false;
+        var stopTime = getTime();
+
+        self.ElapsedTime = (stopTime - capturedTime) / 1000;
+    }
+
+    self.reset = function () {
+        isRunning = false;
+        capturedTime = null;
+        self.ElapsedTime = null;
+    }
+}
